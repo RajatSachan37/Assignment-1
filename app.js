ca19e78f640e389c7711ec5930790799d01262dd
RajatSachan <rajatsachan37@gmail.com>
2022-01-31 20:20:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const User = require("./models/userModel");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// const DB = mongodb+srv://Rajat:r15r15@cluster0.s8gqz.mongodb.net/natours?retryWrites=true&w=majority;
+
+mongoose
+  .connect(
+    "mongodb+srv://Rajat:r15r15@cluster0.s8gqz.mongodb.net/assignment1?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB connection successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//   CRUD = create read update delete
+
+app.get("/", (req, res) => {
+  res.send("Assignment 1");
+});
+
+// CreateUser route
+app.post("/api/v1/users", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    // console.log(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
+
+// ReadAllUsers route
+app.get("/api/v1/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users: users,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
+
+// GetUser by id Route
+app.get("/api/v1/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
+
+// UpdateUser by id
+app.patch("/api/v1/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      updatedData: {
+        user: user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
+
+// DeleteUser by id
+app.delete("/api/v1/users/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: `Student with id: ${req.params.id} has been deleted`,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Student's name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Student's Email is required"],
+    unique: true,
+    lowercase: true,
+  },
+  contact: {
+    type: Number,
+    required: [true, "Student's contact number is required"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
